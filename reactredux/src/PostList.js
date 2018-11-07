0d02bb89fbf00d56d151c649aa16dd9c01d34e7a
梁素玲 <ryousorei@liangsulings-MacBook-Pro.local>
2018-11-07 11:14:37 +0800

    bbs chapter2 props

---
+++ reactredux/src/PostList.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+class PostList extends Component{
+	render(){
+		return(
+			<div>
+				<ul>
+					<li>haha</li>
+					<li>heihei</li>
+					<li>xixi</li>
+				</ul>
+			</div>
+		)
+	}
+}
+export default PostList;
